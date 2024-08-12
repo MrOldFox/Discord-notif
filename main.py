2b8d6a7d72d287e92a8db92b4a726e7fcbaf3772
OldFox <mrolldfox@gmail.com>
2024-08-12 20:08:16 +0500

    New

--- main.py
+++ main.py
@@ -46,15 +46,15 @@ def save_participant_to_json(member):
 
     file_path = 'participants.json'
     if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
     else:
         data = []
 
     data.append(participant_data)
 
-    with open(file_path, 'w') as f:
-        json.dump(data, f, indent=4)
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 # Обработчик команды /join
 @client.event
